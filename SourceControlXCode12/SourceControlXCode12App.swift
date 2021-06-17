373e7b9fd3fe5b47313f082592cc98dee46c30f0
Jesse <Jesse.C.Figueroa@gmail.com>
2021-06-16 23:34:46 -0400

    Initial Commit

---
+++ SourceControlXCode12/SourceControlXCode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXCode12App.swift
+//  SourceControlXCode12
+//
+//  Created by Figueroa, Jesse on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXCode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
